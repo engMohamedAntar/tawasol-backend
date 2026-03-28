cc61c3055c4c3562d639faad932add0e930eea0a
MohamedAntar <aboantar852003@gmail.com>
2026-03-28 20:31:46 +0200

    connected to DB

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/dbConnection');
 require('dotenv').config();
 
 const app= express();
+connectDB();
+
+app.use('/api/auth', require('./routes/authRoute'));
+app.use('/api/posts', require('./routes/postsRoute'));
+app.use('/api/users', require('./routes/usersRoute'));
+app.use('/api/profile', require('./routes/profileRoute'));
 
 const PORT= process.env.PORT || 4000;
-
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`)); 
